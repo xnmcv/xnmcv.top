2eafcc9d0f16fa51c52cb5a6d594408201ba5a6e
xnmcv <87141557+xnmcv@users.noreply.github.com>
2021-07-08 09:53:42 -0700

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1000},"pages":[{"id":"page-index","displayName":"index","link":{"linkType":"LinkTypePage","href":"#!page-index"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
